41ca5157dbc63084a326508a108f01f41f7e6f48
Eduardo Dahmer Correa <80227515+devdahmer99@users.noreply.github.com>
2021-06-28 14:38:16 -0300

    updated file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import SearchBox from './components/SearchBox';
 
 function App() {
+  const [searchText, setSearchText] = useState('');
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    setList([
+      {
+        id:123,
+        title: "Comprar o Bolo",
+        done: false,
+      },
+      {
+        title: "Pegar o dog no PetShop",
+        done: false,
+      },
+      {
+        title: "Gravar Aula",
+        done: true,
+      }
+    ]);
+  }, []);
+
+  function handleSearchInput(newText) {
+    setSearchText(newText);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Lista de Itens</h1>
+
+      <SearchBox 
+      frasePadrao="Adicione um Item..."
+      />
+
+      <hr/>
+      <ul>
+        {list.map((item, index) => (
+          <li key={index}>
+            {item.done &&
+              <del>{item.title}</del>
+            }
+            {!item.done &&
+              item.title
+            }
+            </li>
+        ))}
+      </ul>
+
+    </>
   );
 }
 
